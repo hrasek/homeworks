5dbc376f1aa7d55a3e123fe3e538f2d0a3f571a0
hrasek <84133573+hrasek@users.noreply.github.com>
2021-10-03 22:15:59 +0200

    Update bulls_and_cows.py

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -7,7 +7,7 @@ Created on Mon Jul 19 14:55:46 2021
 import random
 import time
 
-## Vyhodnocuje počet správných tipů
+# Vyhodnocuje počet správných tipů
 def kolo_hry(tip):
     bulls = 0
     cows = 0
@@ -20,7 +20,7 @@ def kolo_hry(tip):
             pass
     return [bulls, cows]
 
-## Vyhodnocuje korektnost tipu
+# Vyhodnocuje korektnost tipu
 def pravidla(tip):
     for cislo in tip:
         if not(cislo.isnumeric()):
@@ -30,50 +30,51 @@ def pravidla(tip):
     if len(set(tip)) != 4:
         return False
     return True
-
-
-start = time.time() # zacatek mereni casu
-# Úvod
-print('Hi there!')
-print(54*'-')
-print("I've generated a random 4 digit number for you.")
-print("Let's play a bulls and cows game.")
-# Generování tipovaného čísla
-cislo_hadane = []
-seznam_cisel = list(range(1,10))  
-for _ in range(4):
-    vyber = random.randint(0,len(seznam_cisel)-1)
-    cislo_hadane.append(seznam_cisel.pop(vyber))
-# Samotná hra
-pocet_tipu = 0
-while True:
-    # Tip uživatele
-    pocet_tipu += 1
-    tip = list(input('Enter a number: '))
+def main():
+    start = time.time() # zacatek mereni casu
+    # Úvod
+    print('Hi there!')
     print(54*'-')
-    # Vyhodnocení, zda tip odpovídá pravidlům.
-    if not pravidla(tip):
-        print('Your tip is against the rules.')
-        print('Try again.')
-        continue
-    # Vyhodnocení počtu bulls and cows
-    bulls, cows = kolo_hry(tip)
-    # Vyhodnocení, zda došlo k výhře
-    if bulls == 4:
-        print("Correct, you've guessed the right number in {} guesses!".format(pocet_tipu))
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    # Generování tipovaného čísla
+    cislo_hadane = []
+    seznam_cisel = list(range(1,10))  
+    for _ in range(4):
+        vyber = random.randint(0,len(seznam_cisel)-1)
+        cislo_hadane.append(seznam_cisel.pop(vyber))
+    # Samotná hra
+    pocet_tipu = 0
+    while True:
+        # Tip uživatele
+        pocet_tipu += 1
+        tip = list(input('Enter a number: '))
         print(54*'-')
-        break
-    # Výpis počtu bulls and cows 
-    else:
-        if bulls == 1 and cows == 1:
-            print('{} bull, {} cow.'.format(bulls,cows))
-        elif bulls == 1:
-            print('{} bull, {} cows.'.format(bulls,cows))
-        elif cows == 1:
-            print('{} bulls, {} cow.'.format(bulls,cows))
+        # Vyhodnocení, zda tip odpovídá pravidlům.
+        if not pravidla(tip):
+            print('Your tip is against the rules.')
+            print('Try again.')
+            continue
+        # Vyhodnocení počtu bulls and cows
+        bulls, cows = kolo_hry(tip)
+        # Vyhodnocení, zda došlo k výhře
+        if bulls == 4:
+            print("Correct, you've guessed the right number in {} guesses!".format(pocet_tipu))
+            print(54*'-')
+            break
+        # Výpis počtu bulls and cows 
         else:
-            print('{} bulls, {} cows.'.format(bulls,cows))
-            
-end = time.time() # Konec měření času
-print('You finished the game in {0:.5} seconds'.format(end-start))
-        
+            if bulls == 1 and cows == 1:
+                print('{} bull, {} cow.'.format(bulls,cows))
+            elif bulls == 1:
+                print('{} bull, {} cows.'.format(bulls,cows))
+            elif cows == 1:
+                print('{} bulls, {} cow.'.format(bulls,cows))
+            else:
+                print('{} bulls, {} cows.'.format(bulls,cows))
+
+    end = time.time() # Konec měření času
+    print('You finished the game in {0:.5} seconds'.format(end-start))
+    
+if __name__ == '__main__':
+    main()
